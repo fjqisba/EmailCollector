0177c280b18b70de1daacfaa6694038eb5a3dfad
fjqisba <fjqisba@sohu.com>
2023-06-12 21:12:21 +0800

    update content

--- Module/EmailMiner/EmailMiner.go
+++ Module/EmailMiner/EmailMiner.go
@@ -13,14 +13,124 @@ import (
 
 
 var(
+	//遇到这些网站就过滤
 	blackDomainMap = map[string]struct{}{
-		"facebook.com":{},
 		"twitter.com":{},
 		"huawei.com":{},
+		"youtube.com":{},
+		"snapchat.com":{},
+		"instagram.com":{},
+		"business.site":{},
+	}
+	//遇到这些网站不用猜其它路径
+	locationDomainMap = map[string]struct{}{
+		"dhl.com":{},
+		"facebook.com":{},
 	}
 	pathList = []string{
-		"contact","contact-us",
+		"contactar",
+		"contactez",
+		"contacter",
+		"aloqa",
+		"contatti",
+		"contact",
+		"contacte",
+		"contacto",
+		"contato",
+		"contatto",
+		"cysylltwch",
+		"fifandraisana",
+		"hubungan",
+		"hubungi",
+		"ikopanya",
+		"kapcsolatba",
+		"Kontak",
+		"kontakt",
+		"kontakta",
+		"kontaktas",
+		"kontakts",
+		"kontaktua",
+		"kukhudzana",
+		"kuntatt",
+		"lamba",
+		"makipag-ugnay",
+		"oxhumana",
+		"stik",
+		"temas",
+		"wasiliana",
+		"contact-us",
+		"xiriir",
+		"iletisim",
+		"bizeulasin",
+		"adborth",
+		"aiseolas",
+		"atsauksmes",
+		"Atsiliepimas",
+		"balik",
+		"bildirim",
+		"comentarios",
+		"comentaris",
+		"feedback",
+		"feed-back",
+		"fidbak",
+		"informacije",
+		"informacje",
+		"information",
+		"iritzia",
+		"maklum",
+		"maoni",
+		"ndemanga",
+		"nzaghachi",
+		"palaute",
+		"parere",
+		"reagim",
+		"rispons",
+		"risposta",
+		"tagasiside",
+		"Tanggepan",
+		"terugkoppeling",
+		"terugvoer",
+		"tilbakemeldinger",
+		"tlhahiso",
+		"urupare",
+		"afdruk",
+		"aftryk",
+		"akara",
+		"alama",
+		"anprent",
+		"avtryck",
+		"avtrykk",
+		"aztarna",
+		"bosma",
+		"damga",
+		"empremta",
+		"Impressum",
+		"impresszum",
+		"imprima",
+		"imprimer",
+		"imprimir",
+		"imprint",
+		"impronta",
+		"Isamisi",
+		"istampar",
+		"izdruka",
+		"jejak",
+		"kaluaran",
+		"marika",
+		"mongolo",
+		"odcisk",
+		"odtis",
+		"otisak",
+		"otisk",
+		"printiad",
+		"riix",
+		"shafi",
+		"spaudas",
+		"tohu",
+		"zolemba",
+		"hakkimizda",
 	}
+	suffixList = []string{".webp",".gif",".htm",".html",".jpeg",".jpg",".php",".png", ".ace",".ani",".arc",".arj",".avi",".bmp",".cab",".class",".css",".exe",".ico",".jar",".mid",".mov",".mp2",".mp3",".mpeg",".mpg",".pdf",".wix"}
 	regex_Email = regexp.MustCompile(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}`)
 )
 
@@ -29,15 +139,15 @@ type EmailMiner struct {
 	mutex sync.Mutex
 	emailList []string
 	emailFilterMap map[string]struct{}
+	concurrencyCh chan struct{}
 }
 
 func filterEmail(emailAddr string)bool  {
 	str := strings.ToLower(emailAddr)
-	if strings.HasSuffix(str,".jpg") == true{
-		return true
-	}
-	if strings.HasSuffix(str,".gif") == true{
-		return true
+	for _,eSuffix := range suffixList{
+		if strings.HasSuffix(str,eSuffix) == true{
+			return true
+		}
 	}
 	if strings.Index(str,"@mail.com") != -1{
 		return true
@@ -45,15 +155,7 @@ func filterEmail(emailAddr string)bool  {
 	if strings.Index(str,"example") != -1{
 		return true
 	}
-	if strings.Index(str,".wix") != -1{
-		return true
-	}
-	if strings.Index(str,".png") != -1{
-		return true
-	}
 	if strings.Index(str,"sentry.io") != -1{
-		return true
-	}else if strings.Contains(str,".webp"){
 		return true
 	}
 	return false
@@ -84,8 +186,9 @@ func GuessEmail(content string)(retList []string)  {
 }
 
 func (this *EmailMiner)exploreUrl(webUrl string)  {
-	log.Println("检测网址:",webUrl)
 	defer this.wg.Done()
+	this.concurrencyCh <- struct{}{}
+	log.Println("检测网址:",webUrl)
 	pageContent := PhantomJS.GetPageHtml(webUrl)
 	emailList := GuessEmail(pageContent)
 	this.mutex.Lock()
@@ -96,11 +199,13 @@ func (this *EmailMiner)exploreUrl(webUrl string)  {
 		}
 	}
 	this.mutex.Unlock()
+	<- this.concurrencyCh
 }
 
 
 func (this *EmailMiner)DetectEmail(webUrl string)[]string {
 	this.emailFilterMap = make(map[string]struct{})
+	this.concurrencyCh = make(chan struct{},3)
 	eUrl,err := url.Parse(webUrl)
 	if err != nil{
 		return nil
@@ -110,14 +215,18 @@ func (this *EmailMiner)DetectEmail(webUrl string)[]string {
 		return nil
 	}
 	if _,bExists := blackDomainMap[eDomain];bExists==true{
+		log.Println("网址域名过滤:",webUrl)
 		return nil
 	}
+	_,bLocation := locationDomainMap[eDomain]
 	this.wg.Add(1)
 	go this.exploreUrl(webUrl)
-	for _,ePath := range pathList {
-		expUrl := fmt.Sprintf("%s://%s/%s",eUrl.Scheme,eUrl.Host,ePath)
-		this.wg.Add(1)
-		go this.exploreUrl(expUrl)
+	if bLocation == false{
+		for _,ePath := range pathList {
+			expUrl := fmt.Sprintf("%s://%s/%s",eUrl.Scheme,eUrl.Host,ePath)
+			this.wg.Add(1)
+			go this.exploreUrl(expUrl)
+		}
 	}
 	this.wg.Wait()
 	return this.emailList
